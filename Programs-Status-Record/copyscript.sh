210bff480ce0b649971c786df5ff48de2a94e909
SamitoX4 <raulbengalysamameascorbe@gmail.com>
2023-02-25 06:14:52 -0500

    Save Dates Insomnia and MoonModeler

---
+++ Programs-Status-Record/copyscript.sh
@@ -0,0 +1,5 @@
+# Install records Insomni from git save
+cp -r /home/blacky/.config/Insomnia/* /home/blacky/VSCODE/After\ Intall\ Ubuntu/After-intall-ubuntu/Programs-Status-Record/InsomniaLocal/
+
+# Install records MoonModeler from git save
+cp -r /home/blacky/.config/MoonModeler/* /home/blacky/VSCODE/After\ Intall\ Ubuntu/After-intall-ubuntu/Programs-Status-Record/MoonModelerLocal/
