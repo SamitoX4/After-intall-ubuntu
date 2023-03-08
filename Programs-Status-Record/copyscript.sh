4525c759ab73d530ad536bbb772208ddb13a9d20
SamitoX4 <raulbengalysamameascorbe@gmail.com>
2023-03-08 12:33:28 -0500

    Add Altair Folder

--- Programs-Status-Record/copyscript.sh
+++ Programs-Status-Record/copyscript.sh
@@ -3,3 +3,6 @@ cp -r /home/blacky/.config/Insomnia/* /home/blacky/VSCODE/After\ Intall\ Ubuntu/
 
 # Install records MoonModeler from git save
 cp -r /home/blacky/.config/MoonModeler/* /home/blacky/VSCODE/After\ Intall\ Ubuntu/After-intall-ubuntu/Programs-Status-Record/MoonModelerLocal/
+
+# Install records Altair from git save
+cp -r /home/blacky/.config/Altair\ GraphQL\ Client/* /home/blacky/VSCODE/After\ Intall\ Ubuntu/After-intall-ubuntu/Programs-Status-Record/AltairLocal/
